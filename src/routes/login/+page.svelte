87a535db89d30d9c9869fd093113f85391430fd5
Oliver Day <oliver.day.swe@gmail.com>
2023-08-22 06:24:48 -0700

    feat: update login helper functions on '/login' route

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,24 @@
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
-    const user = await signInWithPopup(auth, provider);
-    console.log(user);
+    const credential = await signInWithPopup(auth, provider);
+
+    const idToken = await credential.user.getIdToken();
+
+    const res = await fetch('/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ idToken }),
+    });
+  }
+
+  async function signOutSSR() {
+    const res = await fetch('/api/signin', {
+      method: 'DELETE',
+    });
+    await signOut(auth);
   }
 </script>
 
@@ -15,7 +31,7 @@
 {#if $user}
   <h2 class="card-title">Welcome, {$user.displayName}</h2>
   <p class="text-center text-success">You are logged in</p>
-  <button class="btn btn-warning" on:click={() => signOut(auth)}>Sign out</button>
+  <button class="btn btn-warning" on:click={() => signOutSSR}>Sign out</button>
 {:else}
   <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
 {/if}
