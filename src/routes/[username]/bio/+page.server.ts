bd08b9cd833009af71b68d9f7ca2329d600dda99
Oliver Day <oliver.day.swe@gmail.com>
2023-09-09 19:38:18 -0700

    feat: add default action for ''/[username]/bio' route

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,6 +1,6 @@
-import type { PageServerLoad } from "./$types";
-import { adminDB } from "$lib/server/admin";
-import { error, redirect } from "@sveltejs/kit";
+import type { PageServerLoad, Actions } from "./$types";
+import { adminAuth, adminDB } from "$lib/server/admin";
+import { error, fail, redirect } from "@sveltejs/kit";
 
 export const load = (async ({ locals, params }) => {
 
@@ -22,3 +22,28 @@ export const load = (async ({ locals, params }) => {
       bio,
     };
 }) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ locals, request, params }) => {
+
+      const uid = locals.userID;
+
+      const data = await request.formData();
+      const bio = data.get('bio');
+
+      const userRef = adminDB.collection("users").doc(uid!);
+      const { username } = (await userRef.get()).data()!;
+
+      if (params.username !== username) {
+        throw error(401, "That username does not belong to you");
+      }
+
+      if (bio!.length > 260) {
+        return fail(400, { problem: "Bio must be less than 260 characters" });
+      }
+
+      await userRef.update({
+        bio,
+      });
+    },
+  } satisfies Actions;
